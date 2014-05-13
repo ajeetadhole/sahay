011f799aaf49c018b68c981096410164a1af8b8c
ajeetadhole <ajeetadhole@Ajeetas-MacBook-Air.local>
2014-05-12 23:06:27 -0700

    Fixes
    logo, signup flow

--- js/script-ad.js
+++ js/script-ad.js
@@ -32,6 +32,11 @@ $(document).ready(function(){
         height: documentHeight,
     });
 
+    $('.showModalWrapper').css({
+        width: viewportWidth,
+        height: documentHeight,
+    });
+
     $('.tab').hover(
         function(){
             $(this).css({"borderBottom":"solid 3px #3e8dbe"}); 
@@ -84,7 +89,7 @@ $(document).ready(function(){
 
     $('#getStartedButton, #postButton, .editButton').click(
         function(){
-            $('.modalWrapper').css({"display":"block"});
+            $('#candidatePreferencesModal, .modalWrapper').css({"display":"block"});
             $('#candidatePreferencesModal, .modalWrapper').delay(100).animate({opacity:1,},300);
             $('.disabled').animate({"opacity":"0.2"});
             $('.disabled').css({"pointerEvents":"none"});
@@ -99,7 +104,7 @@ $(document).ready(function(){
             $('#postingsTab').css({"borderBottom":"solid 3px #3e8dbe", "opacity":"1"}); 
             $('#sidebar, #sideHeader').animate({"opacity":"0"},75);
             $('#candidatePreferencesModal, .modalWrapper').delay(100).animate({opacity:0,},300);
-            $('.modalWrapper').css({"display":"none"});  
+            $('#candidatePreferencesModal, .modalWrapper').css({"display":"none"});  
         }
     );
 
@@ -202,91 +207,100 @@ $(document).ready(function(){
         mouseY = event.pageY;
     });
 
-    $('.actionSignedOutButtons').hover(
+    $('.actionSignedOutButtons').click(
         function(){
             console.log('foo');
             var offset = $(this).offset();
             $('#contactCard').css({"display":"block", "top":offset.top, "left":offset.left - 275});
             $('#contactCard').animate({"opacity":"1"},300);
-        },
-        function(){
-            console.log('foo');
-            $('#contactCard').delay(1500).animate({"opacity":"0"},300);
-            $('#contactCard').css({"display":"none"});
         }
     );
 
-    $('#postSignedOutButton').hover(
+    $('#postSignedOutButton, .actionSignedOutButtons').click(
         function(){
             console.log('foo');
             var offset = $(this).offset();
             $('#postCard').css({"display":"block", "top":offset.top, "left":offset.left - 275});
             $('#postCard').animate({"opacity":"1"},300);
-        },
-        function(){
-            console.log('foo');
-            $('#postCard').delay(1500).animate({"opacity":"0"},300);
-            $('#postCard').css({"display":"none"});
         }
     );
 
-    $('#nameInput input:text').keydown(
+     $('.hoverCardSecondaryButton').click(
         function(){
             console.log('foo');
-            $('#emailInput').animate({"opacity":"1"},100);   
-            $('#emailInput').css({"pointerEvents":"auto"}); 
+            $('#postCard, #contactCard').delay(1500).animate({"opacity":"0"},300);
+            $('#postCard, #contactCard').css({"display":"none"});
         }
     );
 
-    $('#emailInput input:text').keydown(
+    $('#signUpModal, #paymentsModal').find('input:text').keydown(
         function(){
             console.log('foo');
-            $('#passwordInput').animate({"opacity":"1"},100);   
-            $('#passwordInput').css({"pointerEvents":"auto"}); 
+            $(this).parent().next().animate({"opacity":"1"},100);   
+            $(this).parent().next().css({"pointerEvents":"auto"}); 
         }
     );
 
-    $('#passwordInput input:password').keydown(
+    $('#signUpModal').find('input:password').keydown(
         function(){
             console.log('foo');
-            $('#confirmPasswordInput').animate({"opacity":"1"},100);   
-            $('#confirmPasswordInput').css({"pointerEvents":"auto"}); 
+            $(this).parent().next().animate({"opacity":"1"},100);   
+            $(this).parent().next().css({"pointerEvents":"auto"}); 
         }
     );
 
-    $('#confirmPasswordInput input:password').keydown(
+    $('#signUpModal').find('input:checkbox').change(
         function(){
             console.log('foo');
-            $('#tosInput').animate({"opacity":"1"},100);   
-            $('#tosInput').css({"pointerEvents":"auto"}); 
+            $(this).parent().next().animate({"opacity":"1"},100);   
+            $(this).parent().next().css({"pointerEvents":"auto"}); 
         }
     );
 
-    $('#tosInput input:checkbox').change(
+    $('#tosInput').find('input:checkbox').change(
         function(){
             console.log('foo');
-            $('#signUpButton, #sessionInput').animate({"opacity":"1"},100);   
-            $('#signUpButton, #sessionInput').css({"pointerEvents":"auto"}); 
+            $('#signUpButton').animate({"opacity":"1"},100);   
+            $('#signUpButton').css({"pointerEvents":"auto"}); 
         }
     );
 
-    $('#postSignedOutButton').click(
+    $('#securityInput').find('input:text').keydown(
         function(){
-            $('#signUpModalWrapper').css({"display":"block"});
+            console.log('foo');
+            $('#streetInput').animate({"opacity":"1"},100);   
+            $('#streetInput').css({"pointerEvents":"auto"}); 
+        }
+    );
+
+    $('#postalCodeInput').find('input:text').keydown(
+        function(){
+            console.log('foo');
+            $('#paymentsButton').animate({"opacity":"1"},100);   
+            $('#paymentsButton').css({"pointerEvents":"auto"}); 
+        }
+    );
+
+    $('.hoverCardPrimaryButton').click(
+        function(){
+            $('#signUpModal, #signUpModalWrapper').css({"display":"block"});
             $('#signUpModal, #signUpModalWrapper').delay(100).animate({opacity:1,},300);
             $('.disabled').animate({"opacity":"0.2"});
             $('.disabled').css({"pointerEvents":"none"});
             $('.signUpForm input:radio, .signUpForm input:checkbox').prop( "checked", false );
+            $('.signUpForm #session').prop( "checked", true );
             $('.signUpForm input:text').val("");
+            $('#postCard, #contactCard').delay(1500).animate({"opacity":"0"},300);
+            $('#postCard, #contactCard').css({"display":"none"});
         }
     );
 
     $('#signUpButton').click(
         function(){
-            $('#signUpModalWrapper').css({"display":"none"});
-            $('#signUpModal, #signUpModalWrapper').delay(100).animate({opacity:0,},300);
-            $('#paymentsModalWrapper').css({"display":"block"});
-            $('#paymentsModal, #paymentsModalWrapper').delay(100).animate({opacity:1,},300);
+            $('#signUpModal').delay(100).animate({opacity:0,},300);
+            $('#signUpModal').css({"display":"none"});
+            $('#paymentsModal').css({"display":"block"});
+            $('#paymentsModal').delay(100).animate({opacity:1,},300);
             $('.disabled').animate({"opacity":"0.2"});
             $('.disabled').css({"pointerEvents":"none"});
             $('.paymentsForm input:radio, .paymentsForm input:checkbox').prop( "checked", false );
@@ -296,81 +310,12 @@ $(document).ready(function(){
 
     $('#paymentsButton').click(
         function(){
-            $('#paymentsModalWrapper').css({"display":"none"});
-            $('#paymentsModal, #paymentsModalWrapper').delay(100).animate({opacity:0,},300);
-            $('#candidatePreferencesModalWrapper').css({"display":"block"});
-            $('#candidatePreferencesModal, #candidatePreferencesModalWrapper').delay(100).animate({opacity:1,},300);
+            $('#paymentsModal').delay(100).animate({opacity:0,},300);
+            $('#paymentsModal').css({"display":"none"});
+            $('#candidatePreferencesModal').css({"display":"block"});
+            $('#candidatePreferencesModal').delay(100).animate({opacity:1,},300);
             $('.disabled').animate({"opacity":"0.2"});
             $('.disabled').css({"pointerEvents":"none"});
-            $('.paymentsForm input:radio, .paymentsForm input:checkbox').prop( "checked", false );
-            $('.paymentsForm input:text').val("");
-        }
-    );
-
-    $('#cardNameInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#cardNumberInput').animate({"opacity":"1"},100);   
-            $('#cardNumberInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#cardNumberInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#expiryInput').animate({"opacity":"1"},100);   
-            $('#expiryInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#expiryInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#securityInput').animate({"opacity":"1"},100);   
-            $('#securityInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#securityInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#streetInput').animate({"opacity":"1"},100);   
-            $('#streetInput').css({"pointerEvents":"auto"}); 
-            $('#suburbInput').animate({"opacity":"1"},100);   
-            $('#suburbInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#streetInput input:text').keydown(
-        function(){
-            console.log('foo');
-            console.log('foo');
-            $('#cityInput').animate({"opacity":"1"},100);   
-            $('#cityInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#suburbInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#cityInput').animate({"opacity":"1"},100);   
-            $('#cityInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#cityInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#postalCodeInput').animate({"opacity":"1"},100);   
-            $('#postalCodeInput').css({"pointerEvents":"auto"}); 
-        }
-    );
-
-    $('#postalCodeInput input:text').keydown(
-        function(){
-            console.log('foo');
-            $('#paymentsButton').animate({"opacity":"1"},100);   
-            $('#paymentsButton').css({"pointerEvents":"auto"}); 
         }
     );
 
